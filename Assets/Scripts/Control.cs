967b360d8cc80e00f4908b4fbec83b69c0aa0789
Alessio Arduini <alessio@MBP-di-Alessio.station>
2018-05-13 18:40:36 +0200

    sdsd

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,12 +5,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Control : MonoBehaviour {
     Rigidbody2D rb;
-    public float velocity = 5f;
+
+    [SerializeField]
     float accelerationPower = 5f;
+    [SerializeField]
     float steeringPower = 5f;
     float steeringAMount, speed, direction;
-    float directionX;
-    float directionY;
 
 	// Use this for initialization
 	void Start () {
@@ -18,20 +18,14 @@ public class Control : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-        directionX = CrossPlatformInputManager.GetAxis("Horizontal") * velocity * Time.deltaTime;
-        directionY = CrossPlatformInputManager.GetAxis("Vertical") * velocity * Time.deltaTime;
-
-        //rb.velocity = new Vector2(directionX * 10, directionY); 
-
-        transform.position = new Vector2(transform.position.x + directionX, transform.position.y + directionY);
-
-        steeringAMount = -Input.GetAxis("Horizontal");
-        speed = Input.GetAxis("Vertical") * accelerationPower;
+        steeringAMount = CrossPlatformInputManager.GetAxis("Horizontal");
+        speed = CrossPlatformInputManager.GetAxis("Vertical") * accelerationPower;
         direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
-        rb.rotation += steeringAMount * steeringPower + rb.velocity.magnitude * direction;
+        rb.rotation += steeringAMount * steeringPower * rb.velocity.magnitude * direction;
         rb.AddRelativeForce(Vector2.up * speed);
         rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAMount / 2);
+
 	}
 }
